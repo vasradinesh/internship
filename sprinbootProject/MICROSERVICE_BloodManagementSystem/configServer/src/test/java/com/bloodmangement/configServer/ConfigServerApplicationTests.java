a3baa8bb3a0b741061f3f184c0ce019e4a805a3e
vasradinesh <dbvasra@gmail.com>
2026-04-06 19:00:05 +0530

    create new CONFIG_SERVER project to maintain my all service application properties in on file

---
+++ sprinbootProject/MICROSERVICE_BloodManagementSystem/configServer/src/test/java/com/bloodmangement/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodmangement.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
